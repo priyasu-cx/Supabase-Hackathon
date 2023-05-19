2ca9e3376add4a6dcedf44c31e0a2b48ac78e0c8
priyasu-cx <priyasuguin.stu@gmail.com>
2023-05-19 17:02:47 +0530

    Added Supabase

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,15 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:ConnecTen/firebase_options.dart';
 import 'package:ConnecTen/routes/app_route.dart';
 import 'package:ConnecTen/utils/size_config.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+const supabaseUrl = 'https://ggvpaosmyunvtsxievre.supabase.co';
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdndnBhb3NteXVudnRzeGlldnJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQzOTUzNzUsImV4cCI6MTk5OTk3MTM3NX0.TN-N73C7Nc8JdVXJpwHvCx3ENegqr1dwtdYY_DjI_-I";
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  await Supabase.initialize(url: supabaseUrl, anonKey: supabaseKey);
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
